d1874a2752066c7778bb1806c474a08aad44360d
Valerio-Colombo <valecolomb96@gmail.com>
2018-05-06 19:05:22 +0200

    Added test to CardController and new utility class

---
+++ src/test/java/it/polimi/ingsw/cards/PrivateObjectiveDealerTest.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.cards;
+
+import it.polimi.ingsw.sagrada.game.cards.CardController;
+import it.polimi.ingsw.sagrada.game.cards.CardType;
+import it.polimi.ingsw.sagrada.game.cards.ObjectiveCard;
+import org.junit.Test;
+
+import java.awt.*;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ObjectiveDealerTest {
+
+    @Test
+    public void testPrivateDealer() {
+        int numPlayer = 3;
+        Map<Color, ObjectiveCard> privateObjective;
+        CardController c = new CardController();
+
+        privateObjective = c.dealPrivateObjective(numPlayer);
+        assertEquals(privateObjective.size(), numPlayer);
+        for (ObjectiveCard card:privateObjective.values()) {
+            assertEquals(card.getType(), CardType.PRIVATE);
+        }
+
+        numPlayer = 6;
+
+        privateObjective = c.dealPrivateObjective(numPlayer);
+        assertEquals(privateObjective.size(), 0);
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/cards/PublicObjectiveDealerTest.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.cards;
+
+public class PublicObjectiveDealerTest {
+}
