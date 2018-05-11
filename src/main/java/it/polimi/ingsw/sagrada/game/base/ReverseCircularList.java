3b65f9a83e8403e8210cfe2ff36529187de529f3
ValentinaDeda <valentinadeda96@gmail.com>
2018-05-11 11:26:42 +0200

    ReverseCircularList explanation added

--- src/main/java/it/polimi/ingsw/sagrada/game/base/ReverseCircularList.java
+++ src/main/java/it/polimi/ingsw/sagrada/game/base/ReverseCircularList.java
@@ -13,6 +13,13 @@ public class ReverseCircularList<E> extends ArrayList<E> {
         return this.offset;
     }
 
+
+    /*This method reads the whole array from beginning to the end (even starting from an index different from 0)
+    and then from end to the beginning.
+    During the first iteration (index/size is even) the array is read from beginning to end, during the second
+    (index/size is odd) it is read backwards.
+    Offset is used to set the starting index.
+    */
     @Override
     public E get(int index) {
         int n = size();
