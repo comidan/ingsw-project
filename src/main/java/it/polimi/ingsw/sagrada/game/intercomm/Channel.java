d5d6a3c627e55f64ceed09427a254f75b00d834b
Valerio-Colombo <valecolomb96@gmail.com>
2018-05-14 15:42:28 +0200

    Fixed visibility of intercomm package

--- src/main/java/it/polimi/ingsw/sagrada/game/intercomm/Channel.java
+++ src/main/java/it/polimi/ingsw/sagrada/game/intercomm/Channel.java
@@ -1,5 +1,5 @@
 package it.polimi.ingsw.sagrada.game.intercomm;
 
 public interface Channel<T extends Message> {
-    public void dispatch(T message);
+    void dispatch(T message);
 }
--- src/main/java/it/polimi/ingsw/sagrada/game/intercomm/DynamicRouter.java
+++ src/main/java/it/polimi/ingsw/sagrada/game/intercomm/DynamicRouter.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.sagrada.game.intercomm;
 
 public interface DynamicRouter<T extends Message> {
-    public void subscribeChannel(Class<? extends T> contentType, Channel<? extends T> channel);
-    public abstract void dispatch(T content);
+    void subscribeChannel(Class<? extends T> contentType, Channel<? extends T> channel);
+    void dispatch(T content);
 }
