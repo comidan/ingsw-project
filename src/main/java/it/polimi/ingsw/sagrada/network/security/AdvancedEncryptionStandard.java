0b00846e56e8f8c23b4ee90967d3dfde5cbb8dc4
Daniele Comi <daniele1.comi@mail.polimi.it>
2018-05-16 22:11:39 +0200

    Commit of security module containing AES algorithm and MD5

---
+++ src/main/java/it/polimi/ingsw/sagrada/network/security/AdvancedEncryptionStandard.java
@@ -0,0 +1,76 @@
+package it.polimi.ingsw.sagrada.network.security;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class AdvancedEncryptionStandard {
+
+    private static final Logger LOGGER = Logger.getLogger(AdvancedEncryptionStandard.class.getName());
+    private static final String KEY = "Bar12345Bar12345"; // WARNING : PLEASE FIND A MORE SECURE WAY
+    private static String INIT_VECTOR = "";
+
+    private static String encrypt(String key, String initVector, String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+
+            return Base64.encodeBase64String(encrypted);
+        }
+        catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, () -> "AES encryption fatal error");
+        }
+
+        return null;
+    }
+
+    private static String decrypt(String key, String initVector, String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+            return new String(original);
+        }
+        catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, () -> "AES encryption fatal error");
+        }
+
+        return null;
+    }
+
+    static String getEncryptedData(String data) {
+        INIT_VECTOR = generateInitializationVector();
+        return encrypt(KEY, INIT_VECTOR, data);
+    }
+
+    static String getDecryptedData(String encryptedData) {
+        if(INIT_VECTOR.isEmpty())
+            INIT_VECTOR = generateInitializationVector();
+        String decrypted = decrypt(KEY, INIT_VECTOR, encryptedData);
+        INIT_VECTOR = "";
+        return decrypted;
+    }
+
+    private static String generateInitializationVector() {
+        String stringDate = new Date().getTime()+"";
+        StringBuilder reverse = new StringBuilder();
+        for(int index = stringDate.length() - 1; index >= 0; index--)
+            reverse.append(stringDate.charAt(index));
+        return Security.generateMD5Hash(reverse.toString()).substring(0, 16);
+    }
+}
