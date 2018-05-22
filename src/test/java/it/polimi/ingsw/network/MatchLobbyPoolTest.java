0d429f03bcf488b0bfd1621352aa34f767129cdb
ValentinaDeda <valentinadeda96@gmail.com>
2018-05-22 09:50:37 +0200

    network tool test update

---
+++ src/test/java/it/polimi/ingsw/network/MatchLobbyPoolTest.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.network;
+
+import it.polimi.ingsw.sagrada.network.server.tools.MatchLobby;
+import it.polimi.ingsw.sagrada.network.server.tools.MatchLobbyPool;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+public class MatchLobbyPoolTest {
+    MatchLobbyPool matchLobbyPool;
+
+
+    @Test
+    public void testMatchLobby() {
+        matchLobbyPool = new MatchLobbyPool();
+        try {
+            MatchLobby matchLobby = (matchLobbyPool.getAvailableLobby());
+            assertNotNull(matchLobby);
+            assertFalse(matchLobby.isFull());
+            assertTrue(matchLobbyPool.releaseLobby(matchLobby));
+
+        } catch (IOException exc) {
+
+        }
+
+
+    }
+}
