e9fcb1aed0446c2663b9b870c2d7594e263a5ed1
Valerio-Colombo <valecolomb96@gmail.com>
2018-05-29 00:01:03 +0200

    Fix

--- src/main/java/it/polimi/ingsw/sagrada/game/base/GameManager.java
+++ src/main/java/it/polimi/ingsw/sagrada/game/base/GameManager.java
@@ -152,8 +152,8 @@ public class GameManager implements Channel<Message, Message> {
     private void setDiceInWindow(String idPlayer, Dice dice, Position position) {
         Window window = idToPlayer(idPlayer).getWindow();
         window.setCell(dice, position.getRow(), position.getCol());
-        ErrorType errorType = ruleManager.validateWindow(window.getCellMatrix());
-        System.out.println(errorType);
+        //ErrorType errorType = ruleManager.validateWindow(window.getCellMatrix());
+        //System.out.println(errorType);
     }
 
 
