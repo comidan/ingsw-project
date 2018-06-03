da696321b5365f3c79545f9bd3d6b4702150912e
ValentinaDeda <valentinadeda96@gmail.com>
2018-06-03 16:13:11 +0200

    more window images created

--- src/main/java/it/polimi/ingsw/sagrada/gui/CardBoard.java
+++ src/main/java/it/polimi/ingsw/sagrada/gui/CardBoard.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.sagrada.gui;
 
 
+import it.polimi.ingsw.sagrada.game.cards.ObjectiveCard;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -11,16 +12,25 @@ import java.util.Random;
 
 public class CardBoard extends GridPane {
     List<ToolCardView> toolCardViewList;
+    List<ObjectiveCardView> objectiveCardViewList;
     Resizer resizer;
 
     public CardBoard() {
         toolCardViewList = new ArrayList<>();
+        objectiveCardViewList = new ArrayList<>();
         resizer = new Resizer();
         Random rand = new Random();
         for (int i = 1; i <= 3; i++) {
             ToolCardView toolCardView = new ToolCardView(rand.nextInt(6)+1);
             this.add(toolCardView, i, 1);
             toolCardViewList.add(toolCardView);
+
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            ObjectiveCardView objectiveCardView = new ObjectiveCardView(rand.nextInt(6)+1);
+            this.add(objectiveCardView, i, 1);
+            objectiveCardViewList.add(objectiveCardView);
 
         }
     }
--- src/main/java/it/polimi/ingsw/sagrada/gui/ObjectiveCardView.java
+++ src/main/java/it/polimi/ingsw/sagrada/gui/ObjectiveCardView.java
@@ -1,7 +1,21 @@
 package it.polimi.ingsw.sagrada.gui;
 
-public class ObjectiveCardView {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.File;
 
+public class ObjectiveCardView extends ImageView {
+
+    private static final String TOOL_IMAGE_ROOT_PATH = "src/main/resources/images/ToolImages/";
+    private int id;
+    private Resizer resizer;
+
+    public ObjectiveCardView(int id) {
+        this.resizer = new Resizer();
+        this.id = id;
+        this.setImage(new Image(new File(TOOL_IMAGE_ROOT_PATH + Integer.toString(id) + ".jpg").toURI().toString(), resizer.getWidthPixel(15), resizer.getHeightPixel(20), true, false));
+
+    }
 
 }
