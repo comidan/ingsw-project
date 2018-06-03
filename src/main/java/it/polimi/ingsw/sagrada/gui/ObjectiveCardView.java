d6db874d8b8daf15f47f3c64f15b039dccdd3e65
ValentinaDeda <valentinadeda96@gmail.com>
2018-06-03 16:17:20 +0200

    small fix to objective card view

--- src/main/java/it/polimi/ingsw/sagrada/gui/ObjectiveCardView.java
+++ src/main/java/it/polimi/ingsw/sagrada/gui/ObjectiveCardView.java
@@ -7,14 +7,14 @@ import java.io.File;
 
 public class ObjectiveCardView extends ImageView {
 
-    private static final String TOOL_IMAGE_ROOT_PATH = "src/main/resources/images/ToolImages/";
+    private static final String OBJECTIVE_IMAGE_ROOT_PATH = "src/main/resources/images/ObjectiveImages/";
     private int id;
     private Resizer resizer;
 
     public ObjectiveCardView(int id) {
         this.resizer = new Resizer();
         this.id = id;
-        this.setImage(new Image(new File(TOOL_IMAGE_ROOT_PATH + Integer.toString(id) + ".jpg").toURI().toString(), resizer.getWidthPixel(15), resizer.getHeightPixel(20), true, false));
+        this.setImage(new Image(new File(OBJECTIVE_IMAGE_ROOT_PATH + Integer.toString(id) + ".jpg").toURI().toString(), resizer.getWidthPixel(15), resizer.getHeightPixel(20), true, false));
 
     }
 
