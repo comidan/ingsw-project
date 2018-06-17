c84c646eb76c803f5c2c87601d14ec70ff1929da
ValentinaDeda <valentinadeda96@gmail.com>
2018-06-17 12:19:42 +0200

    error fixed

--- src/test/java/it/polimi/ingsw/gui/GUIManagerTest.java
+++ src/test/java/it/polimi/ingsw/gui/GUIManagerTest.java
@@ -17,11 +17,11 @@ public class GUIManagerTest {
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         assertEquals(gd.getDisplayMode().getHeight() * RELATIVE_DIMENSION, GUIManager.getWindowHeight(), 0);
         assertEquals(gd.getDisplayMode().getHeight() * RELATIVE_DIMENSION * RATIO, GUIManager.getWindowWidth(), 0);
-        assertEquals(gd.getDisplayMode().getHeight(), GUIManager.getScreenHeight(), 0);
+        assertEquals(gd.getDisplayMode().getHeight() * 0.9, GUIManager.getScreenHeight(), 0);
         assertEquals(gd.getDisplayMode().getWidth(), GUIManager.getScreenWidth(), 0);
         int x = new Random().nextInt() % 100;
         assertEquals(x * GUIManager.getWindowHeight() / 100, GUIManager.getHeightPixel(x), 0);
         assertEquals(x * GUIManager.getWindowWidth() / 100, GUIManager.getWidthPixel(x), 0);
-        assertEquals(((double)x) * gd.getDisplayMode().getHeight() / 1080, GUIManager.getResizedFont(x), 0);
+        assertEquals(((double)x) * gd.getDisplayMode().getHeight() * 0.9 / 1080, GUIManager.getResizedFont(x), 0);
     }
 }
