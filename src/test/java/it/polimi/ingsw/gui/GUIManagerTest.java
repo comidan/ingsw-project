b1133f69bf94375897186271c0b1fd16493597a2
Daniele-Comi <superdani96@gmail.com>
2018-06-17 19:40:42 +0200

    Attempting to resolved test approximation issue due to IEEE 754

--- src/test/java/it/polimi/ingsw/gui/GUIManagerTest.java
+++ src/test/java/it/polimi/ingsw/gui/GUIManagerTest.java
@@ -4,24 +4,27 @@ import it.polimi.ingsw.sagrada.gui.utils.GUIManager;
 import org.junit.Test;
 
 import java.awt.*;
+import java.text.DecimalFormat;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
 public class GUIManagerTest {
 
+    /** DecimalFormat usage due to known issue revolving floating point computing approximation described in IEEE 754 */
+
     @Test
     public void testGUIManager() {
         double RATIO = 0.76;
         double RELATIVE_DIMENSION = 0.8;
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        assertEquals(gd.getDisplayMode().getHeight() * RELATIVE_DIMENSION, GUIManager.getWindowHeight(), 0);
-        assertEquals(gd.getDisplayMode().getHeight() * RELATIVE_DIMENSION * RATIO, GUIManager.getWindowWidth(), 0);
-        assertEquals(gd.getDisplayMode().getHeight() * 0.9, GUIManager.getScreenHeight(), 0);
+        assertEquals(Double.parseDouble(new DecimalFormat("#.00").format(gd.getDisplayMode().getHeight() * RELATIVE_DIMENSION).replace(',', '.')), GUIManager.getWindowHeight(), 0);
+        assertEquals(Double.parseDouble(new DecimalFormat("#.00").format(gd.getDisplayMode().getHeight() * RELATIVE_DIMENSION * RATIO).replace(',', '.')), GUIManager.getWindowWidth(), 0);
+        assertEquals(Double.parseDouble(new DecimalFormat("#.00").format(gd.getDisplayMode().getHeight() * 0.9).replace(',', '.')), GUIManager.getScreenHeight(), 0);
         assertEquals(gd.getDisplayMode().getWidth(), GUIManager.getScreenWidth(), 0);
         int x = new Random().nextInt() % 100;
-        assertEquals(x * GUIManager.getWindowHeight() / 100, GUIManager.getHeightPixel(x), 0);
-        assertEquals(x * GUIManager.getWindowWidth() / 100, GUIManager.getWidthPixel(x), 0);
-        assertEquals(((double)x) * gd.getDisplayMode().getHeight() * 0.9 / 1080, GUIManager.getResizedFont(x), 0);
+        assertEquals(Double.parseDouble(new DecimalFormat("#.00").format(x * GUIManager.getWindowHeight() / 100).replace(',', '.')), Double.parseDouble(new DecimalFormat("#.00").format(GUIManager.getHeightPixel(x)).replace(',', '.')), 0);
+        assertEquals(Double.parseDouble(new DecimalFormat("#.00").format(x * GUIManager.getWindowWidth() / 100).replace(',', '.')), Double.parseDouble(new DecimalFormat("#.00").format(GUIManager.getWidthPixel(x)).replace(',', '.')), 0);
+        assertEquals(Double.parseDouble(new DecimalFormat("#.00").format(((double)x) * gd.getDisplayMode().getHeight() * 0.9 / 1080).replace(',', '.')), Double.parseDouble(new DecimalFormat("#.00").format(GUIManager.getResizedFont(x)).replace(',', '.')), 0);
     }
 }
