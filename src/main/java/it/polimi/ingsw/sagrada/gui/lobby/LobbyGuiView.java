43743a1cbddae84717cda66f017f3aa0bdcf8e61
Daniele Comi <daniele1.comi@mail.polimi.it>
2018-06-17 17:04:48 +0200

    Added gui lobby player awaiting animation

--- src/main/java/it/polimi/ingsw/sagrada/gui/lobby/LobbyGuiView.java
+++ src/main/java/it/polimi/ingsw/sagrada/gui/lobby/LobbyGuiView.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.sagrada.gui.lobby;
 
 import it.polimi.ingsw.sagrada.gui.utils.GUIManager;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -27,7 +31,7 @@ public class LobbyGuiView {
     private static final Logger LOGGER = Logger.getLogger(LobbyGuiView.class.getName());
     
     /** The Constant WAITING_PLAYER. */
-    private static final String WAITING_PLAYER = "Waiting player...";
+    private static final String WAITING_PLAYER = "Waiting player";
 
     /** The first player. */
     @FXML
@@ -55,6 +59,12 @@ public class LobbyGuiView {
     /** The stage. */
     private static Stage stage;
 
+    /** Animation timelines */
+    private static Timeline firstPlayerTimeline;
+    private static Timeline secondPlayerTimeline;
+    private static Timeline thirdPlayerTimeline;
+    private static Timeline fourthPlayerTimeline;
+
     /**
      * Sets the player.
      *
@@ -64,10 +74,10 @@ public class LobbyGuiView {
         playerShown.add(username);
         int position = playerShown.indexOf(username);
         switch (position) {
-            case 0 : setFirstPlayer(username); break;
-            case 1 : setSecondPlayer(username); break;
-            case 2 : setThirdPlayer(username); break;
-            case 3:  setFourthPlayer(username); break;
+            case 0 : firstPlayerTimeline.stop(); setFirstPlayer(username); break;
+            case 1 : secondPlayerTimeline.stop(); setSecondPlayer(username); break;
+            case 2 : thirdPlayerTimeline.stop(); setThirdPlayer(username); break;
+            case 3:  fourthPlayerTimeline.stop(); setFourthPlayer(username); break;
             default: break;
         }
     }
@@ -81,10 +91,10 @@ public class LobbyGuiView {
         int position = playerShown.indexOf(username);
         playerShown.remove(position);
         switch (position) {
-            case 0 : setFirstPlayer(WAITING_PLAYER); break;
-            case 1 : setSecondPlayer(WAITING_PLAYER); break;
-            case 2 : setThirdPlayer(WAITING_PLAYER); break;
-            case 3:  setFourthPlayer(WAITING_PLAYER); break;
+            case 0 : setFirstPlayer(WAITING_PLAYER); firstPlayerTimeline.play(); break;
+            case 1 : setSecondPlayer(WAITING_PLAYER); secondPlayerTimeline.play(); break;
+            case 2 : setThirdPlayer(WAITING_PLAYER); thirdPlayerTimeline.play(); break;
+            case 3:  setFourthPlayer(WAITING_PLAYER); fourthPlayerTimeline.play(); break;
             default: break;
         }
     }
@@ -171,6 +181,30 @@ public class LobbyGuiView {
             AnchorPane.setBottomAnchor(player4, getHeightPixel(20));
             Label timerLabel = (Label) scene.lookup("#timer");
             AnchorPane.setBottomAnchor(timerLabel, getHeightPixel(10));
+            firstPlayerTimeline = new Timeline (new KeyFrame(Duration.seconds(0.66), evt -> player1.setText(WAITING_PLAYER + ".")),
+                    new KeyFrame(Duration.seconds(1.32), evt -> player1.setText(WAITING_PLAYER + "..")),
+                    new KeyFrame(Duration.seconds(1.98), evt -> player1.setText(WAITING_PLAYER + "...")),
+                    new KeyFrame(Duration.seconds(2.64), evt -> player1.setText(WAITING_PLAYER)));
+            firstPlayerTimeline.setCycleCount(Animation.INDEFINITE);
+            firstPlayerTimeline.play();
+            secondPlayerTimeline = new Timeline(new KeyFrame(Duration.seconds(0.66), evt -> player2.setText(WAITING_PLAYER + ".")),
+                    new KeyFrame(Duration.seconds(1.32), evt -> player2.setText(WAITING_PLAYER + "..")),
+                    new KeyFrame(Duration.seconds(1.98), evt -> player2.setText(WAITING_PLAYER + "...")),
+                    new KeyFrame(Duration.seconds(2.64), evt -> player2.setText(WAITING_PLAYER)));
+            secondPlayerTimeline.setCycleCount(Animation.INDEFINITE);
+            secondPlayerTimeline.play();
+            thirdPlayerTimeline = new Timeline (new KeyFrame(Duration.seconds(0.66), evt -> player3.setText(WAITING_PLAYER + ".")),
+                    new KeyFrame(Duration.seconds(1.32), evt -> player3.setText(WAITING_PLAYER + "..")),
+                    new KeyFrame(Duration.seconds(1.98), evt -> player3.setText(WAITING_PLAYER + "...")),
+                    new KeyFrame(Duration.seconds(2.64), evt -> player3.setText(WAITING_PLAYER)));
+            thirdPlayerTimeline.setCycleCount(Animation.INDEFINITE);
+            thirdPlayerTimeline.play();
+            fourthPlayerTimeline = new Timeline(new KeyFrame(Duration.seconds(0.66), evt -> player4.setText(WAITING_PLAYER + ".")),
+                    new KeyFrame(Duration.seconds(1.32), evt -> player4.setText(WAITING_PLAYER + "..")),
+                    new KeyFrame(Duration.seconds(1.98), evt -> player4.setText(WAITING_PLAYER + "...")),
+                    new KeyFrame(Duration.seconds(2.64), evt -> player4.setText(WAITING_PLAYER)));
+            fourthPlayerTimeline.setCycleCount(Animation.INDEFINITE);
+            fourthPlayerTimeline.play();
             LobbyGuiView.stage = stage;
             stage.setScene(scene);
             return loaderLobby.getController();
