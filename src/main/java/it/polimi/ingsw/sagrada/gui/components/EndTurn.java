2660faca0faace302f899534ef9cd02006b37be8
Daniele Comi <daniele1.comi@mail.polimi.it>
2018-06-17 12:32:16 +0200

    Click button handler coverage fix

--- src/main/java/it/polimi/ingsw/sagrada/gui/components/EndTurn.java
+++ src/main/java/it/polimi/ingsw/sagrada/gui/components/EndTurn.java
@@ -1,29 +1,29 @@
 package it.polimi.ingsw.sagrada.gui.components;
 
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.TextAlignment;
-import jdk.nashorn.internal.ir.EmptyNode;
 
 public class EndTurn extends StackPane {
 
     Button button;
 
     public EndTurn(){
-        ImageView buttonBackground = new ImageView(new Image("/images/button5.png"));
+        ImageView buttonBackground = new ImageView(new Image(EndTurn.class.getResourceAsStream("/images/button5.png")));
         button = new Button("END TURN");
         button.setStyle("-fx-background-color: transparent;");
+        button.setMouseTransparent(true);
+        buttonBackground.setMouseTransparent(true);
         this.getChildren().addAll(buttonBackground, button);
-
+        prefWidthProperty().bind(button.prefWidthProperty());
+        prefHeightProperty().bind(button.prefHeightProperty());
     }
 
     public void setEndTurnHandler(EventHandler<MouseEvent> endTurnEventHandler){
-        button.setOnMouseClicked(endTurnEventHandler);
+        setOnMouseClicked(endTurnEventHandler);
     }
 
 }
